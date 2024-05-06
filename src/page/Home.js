5db98008ecd4c9a885d11ff66a83734e520a61de
chae0Park <cypark022@gmail.com>
2024-05-06 13:20:11 +0900

    adusted image size etc

--- src/page/Home.js
+++ src/page/Home.js
@@ -1,5 +1,4 @@
 import './Home.css';
-import Ivan from '../asset/introduction_img.jpg';
 import Header from '../component/Header';
 
 
@@ -9,17 +8,16 @@ const Home = () => {
             <Header />
             <div className="item item2">
                 <div className="Home_introduction">
-                What is Lorem Ipsum?
+                
+                    <h2>What is Lorem Ipsum?</h2>
                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
                 Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, 
                 when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
                 It has survived not only five centuries, but also the leap into electronic typesetting, 
-                remaining essentially unchanged. It was popularised in the 1960s 
-                with the release of Letraset sheets containing Lorem Ipsum passages, 
-                and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                remaining essentially unchanged.
                 </div>
                 <div className="Home_img">
-                    <img  className='home-img' src={Ivan} alt={Ivan} />
+                    {/* <img  className='home-img' src={Ivan} alt={Ivan} /> */}
                 </div>
             </div>   
         </div>
